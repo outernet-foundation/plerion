12c41d5fe3fcfb14c24bccb8e1591229a02b1cac
Tyler Hatch <tylershatch@gmail.com>
2025-07-23 16:43:52 -0400

    wip

---
+++ cloudbeaver-init/cloudbeaver-init.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+CONFIG_DIR="/opt/cloudbeaver/workspace/GlobalConfiguration/.dbeaver"
+mkdir -p "$CONFIG_DIR"
+
+cat > "$CONFIG_DIR/data-sources.json" << EOF
+{
+  "folders": {},
+  "connections": {
+    "plerion": {
+      "provider": "postgresql",
+      "driver": "postgres-jdbc",
+      "name": "Plerion",
+      "save-password": true,
+      "configuration": {
+        "host": "${POSTGRES_HOST}",
+        "port": "5432",
+        "database": "${POSTGRES_DB}",
+        "user": "${POSTGRES_USER}",
+        "password": "${POSTGRES_PASSWORD}"
+      }
+    }
+  }
+}
+EOF
