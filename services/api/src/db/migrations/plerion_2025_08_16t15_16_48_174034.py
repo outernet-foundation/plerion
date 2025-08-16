1e48ea5095d6c923df5c8ec69e89b53c9e7974b5
tylershatch <821522+tylershatch@users.noreply.github.com>
2025-08-16 15:16:50 +0000

    chore(deploy): update image-lock.json and migrations

---
+++ services/api/src/db/migrations/plerion_2025_08_16t15_16_48_174034.py
@@ -0,0 +1,38 @@
+from piccolo.apps.migrations.auto.migration_manager import MigrationManager
+from piccolo.columns.column_types import Timestamp
+from piccolo.columns.defaults.timestamp import TimestampNow
+from piccolo.columns.indexes import IndexMethod
+
+
+ID = "2025-08-16T15:16:48:174034"
+VERSION = "1.27.1"
+DESCRIPTION = ""
+
+
+async def forwards():
+    manager = MigrationManager(
+        migration_id=ID, app_name="plerion", description=DESCRIPTION
+    )
+
+    manager.add_column(
+        table_class_name="Capture",
+        tablename="capture",
+        column_name="deleteasdfd_at",
+        db_column_name="deleteasdfd_at",
+        column_class_name="Timestamp",
+        column_class=Timestamp,
+        params={
+            "default": TimestampNow(),
+            "null": True,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    return manager
