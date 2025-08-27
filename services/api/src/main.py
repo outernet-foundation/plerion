0b53ec392580571f76144d601a425aff819aea5f
Tyler Hatch <tylershatch@gmail.com>
2025-08-27 16:03:18 -0400

    Add reconstructions router

--- services/api/src/main.py
+++ services/api/src/main.py
@@ -5,6 +5,7 @@ from common.fastapi import create_fastapi_app
 from fastapi import FastAPI
 
 from .routers.captures import router as captures_router
+from .routers.reconstructions import router as reconstructions_router
 from .settings import get_api_settings
 
 settings = get_api_settings()
@@ -34,3 +35,4 @@ app = create_fastapi_app(
 )
 
 app.include_router(captures_router)
+app.include_router(reconstructions_router)
